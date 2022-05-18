885ffd250a3433bdbc5b95ceb02df202ea70ef6e
grahamedwards <grahamhedwards92@gmail.com>
2022-05-18 09:14:06 -0400

    transfer structs and perturb from params.jl to Mtrpls_build for efficiency. Added struct documentation. Fixed perturb.

--- gpmAr_Mtrpls_build.jl
+++ gpmAr_Mtrpls_build.jl
@@ -1,7 +1,77 @@
 ## gpmAr Metropolis functions: construction site
-using LoopVectorization
-## Plot Evolution of proposals
 
+## Parameters & parameter functions for metropolis code.
+
+"""
+```julia
+lNrm(μ::Float64,σ::Float64)
+```
+Immutable struct to describe normally distributed data,
+    reported as mean (`μ`) and 1σ (`σ`)
+"""
+struct Nrm
+    μ::Float64
+    σ::Float64
+end
+
+"""
+```julia
+lNrm(μ::Float64,σ::Float64)
+```
+Immutable struct to describe lognormally distributed data,
+    reported as log-space mean (`μ`) and 1σ (`σ`)
+"""
+struct lNrm
+    μ::Float64
+    σ::Float64
+end
+
+"""
+```julia
+Unf(a::Float64,b::Float64)
+```
+Immurable struct to describe uniformly distributed data,
+    reported as minimum (`a`) and maximum (`b`).
+"""
+struct Unf
+    a::Float64
+    b::Float64
+end
+
+"""
+```julia
+perturb(p::NamedTuple,k::Symbol,n::Number)
+```
+
+Return a NamedTuple identical to `p`,
+with one field (key `k`) changed to the value of `n`.
+Note that `==` identity is preserved only if the
+order of fields in `p` is as below
+
+Fields: `tss,rAlo,R,ta,cAl,Tm,Tc,ρ,Cp,k,tχα,τχα,Fχα,tχβ,τχβ,Fχβ`
+"""
+function perturb(p::NamedTuple,k::Symbol,n::Number)
+    tχβ  = ifelse(k==:tχβ,n,p.tχβ)
+    τχβ  = ifelse(k==:τχβ,n,p.τχβ)
+    Fχβ  = ifelse(k==:Fχβ,n,p.Fχβ)
+    tχα  = ifelse(k==:tχα,n,p.tχα)
+    τχα  = ifelse(k==:τχα,n,p.τχα)
+    Fχα  = ifelse(k==:Fχα,n,p.Fχα)
+    tss  = ifelse(k==:tss,n,p.tss)
+    rAlo = ifelse(k==:rAlo,n,p.rAlo)
+    R    = ifelse(k==:R,n,p.R)
+    ta   = ifelse(k==:ta,n,p.ta)
+    cAl  = ifelse(k==:cAl,n,p.cAl)
+    Tm   = ifelse(k==:Tm,n,p.Tm)
+    Tc   = ifelse(k==:Tc,n,p.Tc)
+    ρ    = ifelse(k==:ρ,n,p.ρ)
+    Cp   = ifelse(k==:Cp,n,p.Cp)
+    k    = ifelse(k==:k,n,p.k)
+    (; tss,rAlo,R,ta,cAl,Tm,Tc,ρ,Cp,k,tχα,τχα,Fχα,tχβ,τχβ,Fχβ)
+end
+
+
+## Plot Evolution of proposals
 function plotproposals(d::Dict,plims::NamedTuple,cols::Integer;vars::Tuple=(),
                         ll::Bool=true,bounds::Bool=true)
     isempty(vars) ? v=keys(plims) : v=vars
--- plntsml_thrm_params.jl
+++ plntsml_thrm_params.jl
@@ -1,39 +1,59 @@
-## Parameters for planetesimal thermal model
-# Normally distributed data, reported as mean and 1σ
+## Parameters & parameter functions for planetesimal thermal model & metropolis code.
+
+"""
+```julia
+lNrm(μ::Float64,σ::Float64)
+```
+Immutable struct to describe normally distributed data,
+    reported as mean (`μ`) and 1σ (`σ`)
+"""
 struct Nrm
     μ::Float64
     σ::Float64
 end
-# Normally distributed data, reported as mean and 1σ
+
+"""
+```julia
+lNrm(μ::Float64,σ::Float64)
+```
+Immutable struct to describe lognormally distributed data,
+    reported as log-space mean (`μ`) and 1σ (`σ`)
+"""
 struct lNrm
     μ::Float64
     σ::Float64
 end
 
-# Uniformly distributed data
+"""
+```julia
+Unf(a::Float64,b::Float64)
+```
+Immurable struct to describe uniformly distributed data,
+    reported as minimum (`a`) and maximum (`b`).
+"""
 struct Unf
     a::Float64
     b::Float64
 end
 
 """
+```julia
 perturb(p::NamedTuple,k::Symbol,n::Number)
+```
+Return a NamedTuple identical to `p`,
+with one field (key `k`) changed to the value of `n`.
+Note that `==` identity is preserved only if the
+order of fields in `p` is as below
 
-Returns a NamedTuple identical* to `p`,
-with one field (key `k`) changed to `n`
-
-*as long as the order of fields in p is as below.
-
-Fields: tss,rAlo,R,ta,cAl,Tm,Tc,ρ,Cp,k,tχ,τχ,Fχ
+Fields: `tss,rAlo,R,ta,cAl,Tm,Tc,ρ,Cp,k,tχα,τχα,Fχα,tχβ,τχβ,Fχβ`
 """
-
 function perturb(p::NamedTuple,k::Symbol,n::Number)
-    tχβ  = ifelse(k==:tχ,n,p.tχβ)
-    τχβ  = ifelse(k==:τχ,n,p.τχβ)
-    Fχβ  = ifelse(k==:Fχ,n,p.Fχβ)
-    tχα  = ifelse(k==:tχ,n,p.tχα)
-    τχα  = ifelse(k==:τχ,n,p.τχα)
-    Fχα  = ifelse(k==:Fχ,n,p.Fχα)
+    tχβ  = ifelse(k==:tχβ,n,p.tχβ)
+    τχβ  = ifelse(k==:τχβ,n,p.τχβ)
+    Fχβ  = ifelse(k==:Fχβ,n,p.Fχβ)
+    tχα  = ifelse(k==:tχα,n,p.tχα)
+    τχα  = ifelse(k==:τχα,n,p.τχα)
+    Fχα  = ifelse(k==:Fχα,n,p.Fχα)
     tss = ifelse(k==:tss,n,p.tss)
     rAlo= ifelse(k==:rAlo,n,p.rAlo)
     R   = ifelse(k==:R,n,p.R)
