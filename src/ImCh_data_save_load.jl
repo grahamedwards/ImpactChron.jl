c5639898269802241dd758b45004b71d1ce19f72
grahamedwards <grahamhedwards92@gmail.com>
2022-06-01 19:30:45 -0400

    fix bugs in csv functions, add documentation

--- src/ImCh_data_save_load.jl
+++ src/ImCh_data_save_load.jl
@@ -1,6 +1,17 @@
-
+# Efficiently save and load data with DelimitedFiles functions.
 
 ## save data to a csv:
+
+"""
+```julia
+nt2csv(filename::String,N::NamedTuple)
+```
+
+Save a `NamedTuple` to a .csv. Accounts for fields of any type.
+For single-element entries, the rest of the array/table is filled with `NaN`s.
+
+see also: `dict2csv`, `data2csv`
+"""
 
 function nt2csv(filename::String,N::NamedTuple)
     kz = keys(N)
@@ -14,7 +25,7 @@ function nt2csv(filename::String,N::NamedTuple)
         elseif isone(length(N[k]))
             A[1,i] = k
             A[2,i] = N[k]
-            A[3:end,i] = NaN
+            A[3:end,i] .= NaN
         else
             println("key $k didn't save correctly")
         end
@@ -22,10 +33,32 @@ function nt2csv(filename::String,N::NamedTuple)
     writedlm(filename,A,',')
 end
 
+"""
+```julia
+dict2csv(filename::String,D::Dict)
+```
+Save a `Dict` to a .csv. Accounts for fields of any type.
+For single-element entries, the rest of the array/table is filled with `NaN`s.
+
+see also: `nt2csv`, `data2csv`
+"""
 function dict2csv(filename::String,D::Dict)
     nt2csv(filename,(; D...))
 end
 
+
+"""
+```julia
+data2csv(filename::String,data)
+```
+
+Save `data` to a .csv file.
+`data` may be a composite type of `Dict` or `NamedTuple` or an `Array`.
+Accepts fields of any type element.
+For single-element entries, the rest of the array/table is filled with `NaN`s.
+
+see also: `nt2csv`, `dict2csv`
+"""
 function data2csv(filename::String,data)
     if isa(data,Dict)
         dict2csv(filename,data)
@@ -38,7 +71,21 @@ function data2csv(filename::String,data)
     end
 end
 
-## Load data from a csv to a NamedTuple
+## Load data from a csv to a NamedTuple or Dict
+
+"""
+```julia
+csv2nt(filename::String;symbol::Bool=true)
+```
+
+Read a .csv file into a `NamedTuple`.
+Assumes columns reflect individual elements, with the first row specifying that element's key.
+For NaN-buffered columns (see `nt2csv`), the `NaN`s are removed, returning a single-element entry.
+
+A `true` value for `symbol` will convert data saved as arrays of `String`s into arrays of `Symbol`s.
+
+see also: `nt2csv`, `csv2dict`
+"""
 function csv2nt(filename::String;symbol::Bool=true)
     A = readdlm(filename,',') #Input Array
     kz = Tuple(Symbol.(A[1,:]))
@@ -61,8 +108,23 @@ function csv2nt(filename::String;symbol::Bool=true)
     return NamedTuple{kz}(pN)
 end
 
+"""
+```julia
+csv2dict(filename::String;symbol::Bool=true)
+```
 
+Read a .csv file into a `NamedTuple`.
+Assumes columns reflect individual entries, with the first row specifying that entry's key.
+For NaN-buffered columns (see `nt2csv`), the `NaN`s are removed, returning a single-element entry.
+
+A `true` value for `symbol` will convert data saved as arrays of `String`s into arrays of `Symbol`s.
+
+see also: `nt2csv`, `csv2dict`
+"""
 function csv2dict(filename::String;symbol::Bool=true)
-    dnt = csv2nt(filename,symbol)
+    dnt = csv2nt(filename,symbol=symbol)
     return Dict(k => dnt[k] for k ∈ keys(dnt))
 end
+
+
+## Add NetCDF capability some day...
