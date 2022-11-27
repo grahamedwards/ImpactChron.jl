0e9841b6e998b52ebceb6a04ea3568ae243d3c56
Graham H. Edwards <grahamhedwards92@gmail.com>
2022-11-27 12:32:25 -0500

    add SafeTestsets and upgrade Manifest

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,4 +5,4 @@ using SafeTestsets # @safetestset, add "using ImpactChron" after each `begin`.
 
 @testset "perturb" begin include("testImCh_parameters.jl") end
 @testset "ImCh_statistics" begin include("testImCh_statistics.jl") end
-@safetestset "thermal" begin using ImpactChron; include("test/testImCh_thermal.jl") end
+@safetestset "thermal" begin using ImpactChron; include("testImCh_thermal.jl") end
