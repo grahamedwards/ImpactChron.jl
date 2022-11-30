20f4cad83dbd8704a19c5aa2674d2a839812a177
Graham H. Edwards <grahamhedwards92@gmail.com>
2022-11-30 13:16:38 -0700

    new test file names in runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,6 +3,6 @@ using Test
 using SafeTestsets # @safetestset, add "using ImpactChron" after each `begin`.
 
 
-@testset "perturb" begin include("testImCh_parameters.jl") end
-@testset "ImCh_statistics" begin include("testImCh_statistics.jl") end
-@safetestset "thermal" begin using ImpactChron; include("testImCh_thermal.jl") end
+@testset "perturb" begin include("testparameters.jl") end
+@testset "ImCh_statistics" begin include("teststatistics.jl") end
+@safetestset "thermal" begin using ImpactChron; include("testthermal.jl") end
