3f84eb348e60c5f7e18666b1bc9d8645222e1b23
Graham H. Edwards <grahamhedwards92@gmail.com>
2022-12-16 14:41:00 -0500

    put isnothing in weight_petro_types! to protect against full meltdown errors

--- src/statistics.jl
+++ src/statistics.jl
@@ -324,7 +324,7 @@ function weight_petro_types!(v::AbstractArray,T::AbstractArray,petrotypes::Petro
    i5 = findfirst(x -> x<=petrotypes.type5.T, T)
 
 # Require all petrologic types to occupy at least one layer
-   if lastindex(T) >= i3 > i4 > i5 > imelt
+   if !isnothing(imelt) && lastindex(T) >= i3 > i4 > i5 > imelt
    # Calculate and apply the conversion between fractional volume of body and fractional abundance in the meteorite record.
       pv3 = petrotypes.type3.p / sum(@view v[i3:lastindex(T)])
       pv4 = petrotypes.type4.p / sum(@view v[i4:i3-1])
